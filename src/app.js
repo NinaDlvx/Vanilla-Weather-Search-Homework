613b55bfaddc3fc8d772379f4238d8fc0478b4a9
NinaDlvx <165060963+NinaDlvx@users.noreply.github.com>
2024-05-01 17:50:35 +0200

    Added Search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
+function updateCity(city) {
+  let apiKey = `a20035dd93a6tbea3b9cff448a2b7eeo`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
+  updateCity(searchInput.value);
+}
+
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchCity);
+
+updateCity("Antwerp");
